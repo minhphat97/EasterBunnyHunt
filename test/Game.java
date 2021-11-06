6ca0de43e1791872f6cd662a0eedca0215794e53
Steven Xia <scx@sfu.ca>
2021-11-05 21:27:33 -0700

    Fix missing import error.

--- test/Game.java
+++ test/Game.java
@@ -1,13 +1,11 @@
 import java.awt.EventQueue;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
-public class Game extends JFrame implements Runnable
-{
-    public Game()
-    {
-
-        ImageIcon image = new ImageIcon("images/tree.png");
-        setIconImage(image.getImage());//sets application icon
+public class Game extends JFrame implements Runnable {
+    public Game() {
+        var image = new ImageIcon("images/tree.png");
+        setIconImage(image.getImage());  // set application icon
         setContentPane(new Maze());
         setTitle("Easter Bunny Hunt");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -16,14 +14,11 @@ public class Game extends JFrame implements Runnable
     }
 
     @Override
-    public void run()
-    {
+    public void run() {
         setVisible(true);
     }
 
-    public static void main(String[] args)
-    {
-
+    public static void main(String[] args) {
         EventQueue.invokeLater(new Game());
     }
 }
