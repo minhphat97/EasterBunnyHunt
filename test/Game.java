a971ab3d422b7c37d7726743d8af503dcb048126
Miles Burkholder <mburkhol@sfu.ca>
2021-11-05 18:43:48 -0700

    added an icon image for the application when the window opens, can update image when it becomes available

--- test/Game.java
+++ test/Game.java
@@ -1,11 +1,14 @@
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
-public class Game extends JFrame implements Runnable 
+public class Game extends JFrame implements Runnable
 {
     public Game()
     {
-        setContentPane(new Maze()); 
+
+        ImageIcon image = new ImageIcon("images/tree.png");
+        setIconImage(image.getImage());//sets application icon
+        setContentPane(new Maze());
         setTitle("Easter Bunny Hunt");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
@@ -18,7 +21,7 @@ public class Game extends JFrame implements Runnable
         setVisible(true);
     }
 
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
 
         EventQueue.invokeLater(new Game());
