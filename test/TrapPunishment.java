af250bede10d957e91e11141c5d5181ca3a5f426
Chun Hei Yiu <chunheiy@sfu.ca>
2021-11-10 14:00:21 -0800

    implemaneted trap

--- test/TrapPunishment.java
+++ test/TrapPunishment.java
@@ -2,24 +2,15 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public class TrapPunishment extends Environment{
-    private static Image imageClose = null;
-    private static Image imageOpen = null;  
-    private static boolean isOpen = true;
+    private static Image image = null;
 
     TrapPunishment() {
         super();
-        if (TrapPunishment.imageClose == null) {
-            TrapPunishment.imageClose = new ImageIcon("images/48_trap_close.png").getImage();
-            TrapPunishment.imageOpen = new ImageIcon("images/48_trap_open.png").getImage();
+        if (TrapPunishment.image == null) {
+            TrapPunishment.image = new ImageIcon("images/48_trap.gif").getImage();
         }
     }
 
-    @Override
-    public Image getImage() {
-        return TrapPunishment.isOpen ? TrapPunishment.imageOpen : TrapPunishment.imageClose;
-    }
 
-    public static void open() { TrapPunishment.isOpen = true; }
-    public static void close() { TrapPunishment.isOpen = false; }
-    public static boolean checkOpen() { return TrapPunishment.isOpen; }
+    public Image getImage() { return TrapPunishment.image; }
 }
