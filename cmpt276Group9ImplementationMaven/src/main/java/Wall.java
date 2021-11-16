7a31f040823c937502522f9dba30a68a354dd959
Miles Burkholder <mburkhol@sfu.ca>
2021-11-16 10:35:46 -0800

    changes image filepath to get from resources according to maven file structure

---
+++ cmpt276Group9ImplementationMaven/src/main/java/Wall.java
@@ -0,0 +1,24 @@
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+
+public class Wall extends Environment {
+	private static Image imageNorm = null;
+    private static Image imageAlt = null;
+
+    // This is the image that is actually displayed.
+    private Image image = null;
+    private static double altPercentage = 0.2;
+
+	Wall() {
+		super();
+		if (Wall.imageNorm == null) {
+			Wall.imageNorm = new ImageIcon("cmpt276Group9ImplementationMaven/src/main/resources/images/48_tree_2.png").getImage();
+            Wall.imageAlt = new ImageIcon("cmpt276Group9ImplementationMaven/src/main/resources/images/48_tree_1.png").getImage();
+        }
+        this.image = Math.random() < Wall.altPercentage ? Wall.imageAlt : Wall.imageNorm;
+	}
+
+	@Override
+	public Image getImage() { return this.image; }
+}
