9f7762780aeb07e754855505b5452671466dda10
Miles Burkholder <mburkhol@sfu.ca>
2021-11-16 10:40:04 -0800

    changes image filepath to get from resources according to maven file structure

---
+++ cmpt276Group9ImplementationMaven/src/main/java/Door.java
@@ -0,0 +1,26 @@
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+
+public class Door extends Environment {
+	private static Image imageClose = null;
+	private static Image imageOpen = null;	
+	private static boolean isOpen = false;
+
+	Door() {
+		super();
+		if (Door.imageClose == null) {
+			Door.imageClose = new ImageIcon("cmpt276Group9ImplementationMaven/src/main/resources/images/48_portal_close.gif").getImage();
+			Door.imageOpen = new ImageIcon("cmpt276Group9ImplementationMaven/src/main/resources/images/48_portal_open.gif").getImage();
+        }
+	}
+
+	@Override
+	public Image getImage() {
+        return Door.isOpen ? Door.imageOpen : Door.imageClose;
+	}
+
+	public static void open() { Door.isOpen = true; }
+    public static void close() { Door.isOpen = false; }
+	public static boolean checkOpen() { return Door.isOpen; }
+}
