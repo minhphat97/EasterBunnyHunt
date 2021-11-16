8938de720044ad50d5659aa9aadf1fd91358abf9
Miles Burkholder <mburkhol@sfu.ca>
2021-11-16 10:37:32 -0800

    changes image filepath to get from resources according to maven file structure

---
+++ cmpt276Group9ImplementationMaven/src/main/java/ScoreBonus.java
@@ -0,0 +1,20 @@
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+
+public class ScoreBonus extends Environment {
+    private static Image image = null;
+
+    // Number range for maximum score bonus (0 to RANGE).
+    private final int RANGE = 5;
+
+    ScoreBonus() {
+        super();
+        if (ScoreBonus.image == null) {
+            ScoreBonus.image = new ImageIcon("cmpt276Group9ImplementationMaven/src/main/resources/images/48_bonus_egg.png").getImage();
+        }
+    }
+
+    @Override
+    public Image getImage() { return ScoreBonus.image; }
+}
