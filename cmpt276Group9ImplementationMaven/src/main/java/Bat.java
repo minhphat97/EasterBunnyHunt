a85c27c538b15150d1afe26fb98675996379a6bc
Miles Burkholder <mburkhol@sfu.ca>
2021-11-16 10:40:20 -0800

    changes image filepath to get from resources according to maven file structure

---
+++ cmpt276Group9ImplementationMaven/src/main/java/Bat.java
@@ -0,0 +1,28 @@
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+
+public class Bat extends Enemy {
+    private static Image image_left, image_right;
+
+    public Bat(int x, int y) {
+        super(x, y);
+        if (Bat.image_left == null) {
+            Bat.image_left = new ImageIcon("cmpt276Group9ImplementationMaven/src/main/resources/images/48_bat_left.gif").getImage();
+            Bat.image_right = new ImageIcon("cmpt276Group9ImplementationMaven/src/main/resources/images/48_bat_right.gif").getImage();
+        }
+
+        this.image_last = Bat.image_left;
+
+        this.turnPercentage = 0.3;
+        this.defaultSpeed = 4;
+        this.speed = this.defaultSpeed;
+    }
+
+    @Override
+    public Image getImage() {
+        if (this.deltaX != 0)
+            this.image_last = this.deltaX < 0 ? Bat.image_left : Bat.image_right;
+        return this.image_last;
+    }
+}
