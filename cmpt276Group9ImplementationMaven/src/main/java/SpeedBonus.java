9b86732700fd19586d5f3e944ac4db5127aa8b79
Miles Burkholder <mburkhol@sfu.ca>
2021-11-16 10:37:19 -0800

    changes image filepath to get from resources according to maven file structure

---
+++ cmpt276Group9ImplementationMaven/src/main/java/SpeedBonus.java
@@ -0,0 +1,16 @@
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class SpeedBonus extends Environment {
+    private static Image image = null;
+
+    SpeedBonus() {
+        super();
+        if (SpeedBonus.image == null) {
+            SpeedBonus.image = new ImageIcon("cmpt276Group9ImplementationMaven/src/main/resources/images/speed_bonus.png").getImage();
+        }
+    }
+
+    @Override
+    public Image getImage() { return SpeedBonus.image; }
+}
